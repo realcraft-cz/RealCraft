74d65923c2e8396ed0c2f5f18fda073cb108c00a
FreeWall <info@michalvanek.net>
2017-06-29 10:26:38 +0200

    RandomSpawn ocean biomes exclusion

--- src/com/realcraft/survival/RandomSpawn.java
+++ src/com/realcraft/survival/RandomSpawn.java
@@ -5,19 +5,22 @@ import java.util.HashMap;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.block.Biome;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
 
 import com.realcraft.RealCraft;
+import com.realcraft.chat.ChatCommandSpy;
 import com.realcraft.utils.LocationUtil;
 import com.realcraft.utils.RandomUtil;
 
 public class RandomSpawn implements Listener {
 
-	private static final int RANDOM_LIMIT = 500*1000;
+	private static final int RANDOM_LIMIT = 300*1000;
 	private static final int RANDOM_SIZE = 5000;
 	private HashMap<Player,Long> lastRandomSpawn = new HashMap<Player,Long>();
 
@@ -36,11 +39,24 @@ public class RandomSpawn implements Listener {
 				return;
 			}
 			lastRandomSpawn.put(player,System.currentTimeMillis());
-			player.teleport(this.getRandomLocation(Bukkit.getWorld("world")));
+			player.teleport(this.getRandomLocation(Bukkit.getWorld("world")),TeleportCause.PLUGIN);
+			ChatCommandSpy.sendCommandMessage(player,command);
 		}
 	}
 
 	public Location getRandomLocation(World world){
-		return LocationUtil.getSafeDestination(new Location(world,RandomUtil.getRandomInteger(-RANDOM_SIZE,RANDOM_SIZE),world.getMaxHeight(),RandomUtil.getRandomInteger(-RANDOM_SIZE,RANDOM_SIZE)));
+		Location location = LocationUtil.getSafeDestination(new Location(world,RandomUtil.getRandomInteger(-RANDOM_SIZE,RANDOM_SIZE),world.getMaxHeight(),RandomUtil.getRandomInteger(-RANDOM_SIZE,RANDOM_SIZE)));
+		if(this.isLocationInOcean(location)) location = this.getRandomLocation(world);
+		return location;
+	}
+
+	public boolean isLocationInOcean(Location location){
+		return (
+			location.getWorld().getBiome(location.getBlockX(),location.getBlockZ()) == Biome.OCEAN || location.getWorld().getBiome(location.getBlockX(),location.getBlockZ()) == Biome.DEEP_OCEAN ||
+			location.getWorld().getBiome(location.getBlockX()+1,location.getBlockZ()) == Biome.OCEAN || location.getWorld().getBiome(location.getBlockX()+1,location.getBlockZ()) == Biome.DEEP_OCEAN ||
+			location.getWorld().getBiome(location.getBlockX()-1,location.getBlockZ()) == Biome.OCEAN || location.getWorld().getBiome(location.getBlockX()-1,location.getBlockZ()) == Biome.DEEP_OCEAN ||
+			location.getWorld().getBiome(location.getBlockX(),location.getBlockZ()+1) == Biome.OCEAN || location.getWorld().getBiome(location.getBlockX(),location.getBlockZ()+1) == Biome.DEEP_OCEAN ||
+			location.getWorld().getBiome(location.getBlockX(),location.getBlockZ()-1) == Biome.OCEAN || location.getWorld().getBiome(location.getBlockX(),location.getBlockZ()-1) == Biome.DEEP_OCEAN
+		);
 	}
 }
